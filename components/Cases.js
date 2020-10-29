c9229394b6b2aa05ea37cbb53f22a5742ed943c4
snazheizenberg <azizsalam148@gmail.com>
2020-10-29 13:50:23 +0000

    cases

--- components/Cases.js
+++ components/Cases.js
@@ -1,11 +1,63 @@
-import React from "react";
-
-import { View, Text } from "react-native"
-
+import React from 'react';
+import { View } from "react-native";
+import { Container, Header, Content, List, ListItem, Text, Icon, Button, Body, Title } from 'native-base';
 export default function Cases() {
     return (
-        <View>
-            <Text>this is the cases place</Text>
-        </View>
-    )
+      <Container>
+        <Header>
+            <Body>
+                <Title style={{color: "#2DC2A9", fontSize: 22}}>Reported Cases</Title>
+          </Body>
+        </Header>
+        <Content>
+          <List>
+            <ListItem style={{flexDirection: "row" ,justifyContent: "space-between"}}>
+              <Text>Accident at Circle</Text>
+              <View>
+                    <Button rounded light style={{width: 81, height: 30, border: "red"}}>
+                    <Text style={{ color: "#fff",textAlign: "center"}}>Track</Text>
+                    </Button>
+                </View>
+              <Icon type="FontAwesome" name="angle-right" />
+            </ListItem>
+            <ListItem style={{flexDirection: "row" ,justifyContent: "space-between"}}>
+              <Text>Accident at Circle</Text>
+              <View >
+                    <Button rounded light style={{width: 81, height: 30, borderColor: "red"}}>
+                    <Text style={{color: "#fff", textAlign: "center"}}>Track</Text>
+                    </Button>
+                </View>
+              <Icon type="FontAwesome" name="angle-right" />
+            </ListItem>
+            <ListItem style={{flexDirection: "row" ,justifyContent: "space-between"}}>
+              <Text>Accident at Dansoman</Text>
+              <View >
+                    <Button rounded light style={{width: 81, height: 30, borderColor: "red"}}>
+                    <Text style={{color: "#fff", textAlign: "center"}}>Track</Text>
+                    </Button>
+                </View>
+              <Icon type="FontAwesome" name="angle-right" />
+            </ListItem>
+            <ListItem style={{flexDirection: "row" ,justifyContent: "space-between"}}>
+              <Text>Accident at Dansoman</Text>
+              <View >
+                    <Button rounded light style={{width: 81, height: 30, borderColor: "red"}}>
+                    <Text style={{color: "#fff", textAlign: "center"}}>Track</Text>
+                    </Button>
+                </View>
+              <Icon type="FontAwesome" name="angle-right" />
+            </ListItem>
+            <ListItem style={{flexDirection: "row" ,justifyContent: "space-between"}}>
+              <Text>Accident at Dansoman</Text>
+              <View >
+                    <Button rounded light style={{width: 81, height: 30, borderColor: "red"}}>
+                    <Text style={{color: "#fff", textAlign: "center"}}>Track</Text>
+                    </Button>
+                </View>
+              <Icon type="FontAwesome" name="angle-right" />
+            </ListItem>
+          </List>
+        </Content>
+      </Container>
+    );
 }
